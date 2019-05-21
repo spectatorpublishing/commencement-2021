ee32f6d8cc741b2e21f5e8611ea21bf6198f5b70
Cremebrutale <caroline.hoang@columbiaspectator.com>
2019-05-21 18:44:58 -0400

    I added the json information in

--- src/utils/GlobalData.js
+++ src/utils/GlobalData.js
@@ -2,19 +2,19 @@ const GlobalData = {
     "Senior Profiles": [
         {
             title: "Aaron Smithson, CC",
-            link: "https://www.reddit.com",
+            link: "https://www.columbiaspectator.com/the-eye/2019/05/21/aaron-smithson-cc-19-just-needs-his-space/",
             author: "Sara Bell",
             img: "http://via.placeholder.com/150x150/"
         },
         {
             title: "Alani Sumako, BC",
-            link: "https://www.reddit.com",
+            link: "https://www.columbiaspectator.com/the-eye/2019/05/21/alani-sumako-fujii-bc-19-practices-public-service-through-self-care/",
             author: "Candy Chan",
             img: "http://via.placeholder.com/150x150/"
         },
         {
             title: "Alexandria Parkhurst, CC",
-            link: "https://www.reddit.com",
+            link: "https://www.columbiaspectator.com/the-eye/2019/05/21/tour-leader-band-leader-business-leader-alex-parkhurst-cc-19-reflects/",
             author: "Mary Marsh",
             img: "http://via.placeholder.com/150x150/"
         },
@@ -26,7 +26,7 @@ const GlobalData = {
         },
         {
             title: "Elizabeth Ulanova, GS",
-            link: "https://www.reddit.com",
+            link: "https://www.columbiaspectator.com/the-eye/2019/05/21/elizabeth-ulanova-gs-19-is-where-art-meets-technology-meets-axe-throwing/",
             author: "Julian Shen-Berro",
             img: "http://via.placeholder.com/150x150/"
         },        
@@ -111,67 +111,128 @@ const GlobalData = {
     ],
     "Senior Columns": [
         {
-            title: "Ya yeet 6",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "How to lose friends and influence people",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/21/how-to-lose-friends-and-influence-people/",
+            author: "Aaron Holmes",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/NA65VXYSKRAUNMJTXZNT3YLNDY.jpg"
         },
         {
-            title: "Ya yeet 7",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "What’s the story?",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/20/whats-the-story/",
+            author: "Ainsley Brandowski",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/2WY34VK4UBEIBJEGO26KFRHXW4.jpg"
         },
         {
-            title: "Ya yeet 8",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "Letting it matter",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/20/letting-it-matter/",
+            author: "Catherine Hoang",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/O6A3P66E3FAEVNG3XYFD7FZY5I.jpg"
         },
         {
-            title: "Ya yeet 9",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "I never meant to fall in love with Spec",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/20/i-never-meant-to-fall-in-love-with-spec/",
+            author: "Sarah Fornshell",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/2PPBK3XRWVB6DA25RAQ3UPV46A.jpg"
         },
         {
-            title: "Ya yeet 10",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "Self-care according to a spec mom",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/20/self-care-according-to-a-spec-mom/",
+            author: "Laura Salgado",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/5IQA45NXTRDIJCZDDUP3LANKQE.jpg"
+        },
+        {
+            title: "Oui Papa, I’m in the library",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/20/oui-papa-im-in-the-library/",
+            author: "Yasmine Akki",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/X6VHVWPHQ5ASDBFE5ZICBX2BWY.jpg"
+        },
+        {
+            title: "Why I wrote ",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/21/why-i-wrote/",
+            author: "Chris Lopez",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/UL37ZLGRA5B7NONOXYX3MHVQ2Y.jpg"
+        },
+        {
+            title: "Communites of Care",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/20/communities-of-care/",
+            author: "Parth Chhabra",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/KZ52ATI54ZHETOK6J3VLNPZY2I.jpg"
+        },
+        {
+            title: "The other side of the glass ceiling",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/21/the-other-side-of-the-glass-ceiling/",
+            author: "JJ Spitz",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/YWIJVIMYOFAGXPIZUI2J6CZ33U.jpg"
+        },
+        {
+            title: "Being a good editor, on and off the page",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/20/being-a-good-editor-on-and-off-the-page/",
+            author: "Hannah Barbosa Cesnik",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/2FFYP5H5TZBXBCVHDUC3HBXO6Y.jpg"
+        },
+        {
+            title: "It's just a newspaper",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/21/its-just-a-newspaper/",
+            author: "Sophie Kossakowski",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/ID6VXS67ENHN5MSPIPLNCEENFQ.jpg"
+        },
+        {
+            title: "Fear is a palpable thing",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/20/fear-is-a-palpable-thing/",
+            author: "Charlotte Pu",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/VMAWNAWZWNA5RLZD4MCEAEQASE.jpg"
+        },
+        {
+            title: "A crash course",
+            link: "https://www.columbiaspectator.com/photo-essay/2019/05/21/a-crash-course/",
+            author: "Elza Bouhassira",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/X444KW4TAVGJHAPIKPLGU2CUGY.jpg"
+        },
+        {
+            title: "Hydration and history",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/21/hydration-and-history/",
+            author: "Austin Horn",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/7RSAH6ZCDNFRBAQIAPQ335YLB4.jpg"
+        },
+        {
+            title: "They don't really care about us",
+            link: "https://www.columbiaspectator.com/opinion/2019/05/21/they-dont-really-care-about-us/",
+            author: "Alex McNab",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/5M3EMMSYG5BFTEXBQBHEXZTK3E.jpg"
         }
     ],
     "Class Days": [
         {
-            title: "Ya yeet 11",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "GS Class Day",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/fashion-model-and-activist-calls-on-graduates-to-advocate-for-positive-change-at-gs-class-day/",
+            author: "Marianna Najman-Franks",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/DTGMVMIPANDXVMLZWP5CGTQ2GM.jpg"
         },
         {
-            title: "Ya yeet 12",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "SEAS Class Day ",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/at-seas-class-day-bloomberg-cto-emphasizes-need-for-engineering-in-a-technology-driven-world/",
+            author: "Valeria Escobar",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/WTVMVDHXGNFMFBOVHI664FYNOM.jpg"
         },
         {
-            title: "Ya yeet 13",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "CC Class Day ",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/broadway-star-brandon-victor-dixon-cc-03-emphasizes-ability-to-inspire-change-at-columbia-college-class-day/",
+            author: "Shubham Saharan",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/7PO7X3AA3FFENDQF3QEWVW4SB4.jpg"
         },
         {
-            title: "Ya yeet 14",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        },
-        {
-            title: "Ya yeet 15",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "Barnard Commencement",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/viola-davis-tells-graduates-that-empathy-fuels-change-at-barnard-commencement/",
+            author: "Marianna Najman-Franks",
+            img: "https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/QFGUTOIJAZEKTL2UD57JCPGEKI.jpg"
         }
+        // ,
+        // {
+        //     title: "Ya yeet 15",
+        //     link: "https://www.reddit.com",
+        //     author: "DJ Thonk",
+        //     img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+        // }
     ]
 }
 export default GlobalData;
