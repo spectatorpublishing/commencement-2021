fbe39a68807693b7ac3e13b3ae56a45e77a4bcee
Rounak Bera <rounakbera@gmail.com>
2019-05-21 17:36:11 -0400

    senior profiles

--- src/utils/GlobalData.js
+++ src/utils/GlobalData.js
@@ -1,34 +1,112 @@
 const GlobalData = {
     "Senior Profiles": [
         {
-            title: "Ya yeet 1",
+            title: "Aaron Smithson, CC",
             link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            author: "Sara Bell",
+            img: "http://via.placeholder.com/150x150/"
         },
         {
-            title: "Ya yeet 2",
+            title: "Alani Sumako, BC",
             link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            author: "Candy Chan",
+            img: "http://via.placeholder.com/150x150/"
         },
         {
-            title: "Ya yeet 3",
+            title: "Alexandria Parkhurst, CC",
             link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            author: "Mary Marsh",
+            img: "http://via.placeholder.com/150x150/"
         },
         {
-            title: "Ya yeet 4",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            title: "Caroline Wallis, BC, and Jacquelyn Klein, CC",
+            link: "https://www.columbiaspectator.com/arts-and-entertainment/2019/05/21/jacquelyn-klein-cc-19-and-caroline-wallis-bc-19-helped-lay-the-bedrock-of-columbias-visual-arts-landscape/",
+            author: "Isabela Espadas Barros Leal",
+            img: "http://via.placeholder.com/150x150/"
         },
         {
-            title: "Ya yeet 5",
+            title: "Elizabeth Ulanova, GS",
             link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
+            author: "Julian Shen-Berro",
+            img: "http://via.placeholder.com/150x150/"
+        },        
+        {
+            title: "Emily Sun, CC",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/from-pre-med-to-medieval-riddles-how-emily-sun-cc-19-found-magic-at-columbia/",
+            author: "Valeria Escobar",
+            img: "http://via.placeholder.com/150x150/"
+        },        
+        {
+            title: "Jed-Joan Edziah, SEAS",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/jed-joan-edziah-seas-19-uncertain-futures-and-assuring-relationships/",
+            author: "Valeria Escobar ",
+            img: "http://via.placeholder.com/150x150/"
+        },        
+        {
+            title: "Jennifer Blandon, GS",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/jennifer-blandon-gs-19-veteran-philosopher-and-leading-veteran/",
+            author: "Emma James",
+            img: "http://via.placeholder.com/150x150/"
+        },  
+        {
+            title: "Kanoa Mendenhall, CC",
+            link: "https://www.columbiaspectator.com/arts-and-entertainment/2019/05/20/kanoa-mendenhall-reflects-on-her-musical-journey-preparing-for-first-international-tour/",
+            author: "Fonda Shen",
+            img: "http://via.placeholder.com/150x150/"
+        },  
+        {
+            title: "Kiana Mendes, CC",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/kiana-mendes-cc-19-connecting-communities-with-compassion/",
+            author: "Emma James",
+            img: "http://via.placeholder.com/150x150/"
+        },    
+        {
+            title: "Rachel Greenfeld, BC",
+            link: "https://www.columbiaspectator.com/arts-and-entertainment/2019/05/21/varsity-show-celebrity-rachel-greenfeld-bc-19-from-start-to-finish/",
+            author: "Samuel Jones",
+            img: "http://via.placeholder.com/150x150/"
+        },    
+        {
+            title: "Robernisha Williams, BC",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/robrenisha-williams-bc-19-politicizing-the-neutral-and-educating-for-better-futures/",
+            author: "Marianna Najman-Franks",
+            img: "http://via.placeholder.com/150x150/"
+        },        
+        {
+            title: "Samuel Castro, SEAS",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/samuel-castro-seas-19-fostering-la-familia-3000-miles-from-home/",
+            author: "Shubham Saharan ",
+            img: "http://via.placeholder.com/150x150/"
+        },
+        {
+            title: "Shaquan Nelson, SEAS",
+            link: "https://www.columbiaspectator.com/arts-and-entertainment/2019/05/21/shaquan-nelson-seas-19-on-creating-his-own-clothing-label-and-how-a-turtle-can-climb-a-fence/",
+            author: "Sarah Beckley",
+            img: "http://via.placeholder.com/150x150/"
+        },
+        {
+            title: "Shreya Sunderram, BC",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/shreya-sunderram-bc-19-activism-in-conversation-action-in-the-classroom-and-radical-love-in-practice/",
+            author: "Valeria Escobar",
+            img: "http://via.placeholder.com/150x150/"
+        },
+        {
+            title: "Sondra Woodruff, GS",
+            link: "https://www.columbiaspectator.com/arts-and-entertainment/2019/05/21/from-the-backstreet-boys-to-butler-sondra-woodruff-gs-19-gained-new-musical-perspectives-through-columbia/",
+            author: "Gia Kim",
+            img: "http://via.placeholder.com/150x150/"
+        },
+        {
+            title: "Valerie Jaharis, BC",
+            link: "https://www.columbiaspectator.com/news/2019/05/21/valerie-jaharis-bc-19-interrogating-space-and-pushing-for-unequivocal-equity/",
+            author: "Marianna Najman-Franks",
+            img: "http://via.placeholder.com/150x150/"
+        },
+        {
+            title: "Victor Pham, CC",
+            link: "https://www.columbiaspectator.com/sports/2019/05/21/victor-pham-cc-19-played-tennis-for-his-family-on-the-court-and-his-family-off-the-court/",
+            author: "Elizabeth Karpen",
+            img: "http://via.placeholder.com/150x150/"
         }
     ],
     "Senior Columns": [
