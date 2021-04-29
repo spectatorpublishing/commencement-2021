170064ed7ce22cd4341d0071b8ffc024677d3e63
Erin <erin2722@gmail.com>
2021-04-28 23:47:57 -0400

    :rainbow:

--- src/utils/GlobalData.js
+++ src/utils/GlobalData.js
@@ -1,36 +1,4 @@
 const GlobalData = {
-    "Senior Profiles": [
-        {
-            title: "Ya yeet 1",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        },
-        {
-            title: "Ya yeet 2",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        },
-        {
-            title: "Ya yeet 3",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        },
-        {
-            title: "Ya yeet 4",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        },
-        {
-            title: "Ya yeet 5",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        }
-    ],
     "Senior Columns": [
         {
             title: "Ya yeet 6",
@@ -58,38 +26,6 @@ const GlobalData = {
         },
         {
             title: "Ya yeet 10",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        }
-    ],
-    "Class Days": [
-        {
-            title: "Ya yeet 11",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        },
-        {
-            title: "Ya yeet 12",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        },
-        {
-            title: "Ya yeet 13",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        },
-        {
-            title: "Ya yeet 14",
-            link: "https://www.reddit.com",
-            author: "DJ Thonk",
-            img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
-        },
-        {
-            title: "Ya yeet 15",
             link: "https://www.reddit.com",
             author: "DJ Thonk",
             img: "https://steamuserimages-a.akamaihd.net/ugc/887630550045153287/7DD753A52C8BB98273532E12463385BE36E85968/"
